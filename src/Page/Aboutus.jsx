8316886f56585cf3eed8166bea6c3fa33a258611
ajinkyaanwade <147714454+ajinkyaanwade@users.noreply.github.com>
2024-03-01 18:13:56 +0530

    Delete src directory

--- src/Page/Aboutus.jsx
+++
@@ -1,9 +0,0 @@
-import Navbar_tool from '../component/Navbar_tool';
-export default function Aboutus(){
-  return(
-    <div> 
-      <Navbar_tool/>
-    <p>this is about us</p>
-    </div>
-  )
-}
--- src/Page/Invoice.jsx
+++
@@ -1,5 +0,0 @@
-export default function Invoice(){
-    return (
-        <div>invoice</div>
-    );
-}
